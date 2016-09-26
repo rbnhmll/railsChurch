041c2e4574c5e51cbc4c37960944b6cfc87db2dd
Robin Hamill <hi@robinhamill.ca>
2016-09-26 15:21:39 -0400

    Add associations to User and Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926191311) do
+ActiveRecord::Schema.define(version: 20160926191920) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160926191311) do
     t.boolean  "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
