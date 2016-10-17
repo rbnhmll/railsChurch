a7fe7bea56817a7f1dc817145a9921db830621ff
Robin Hamill <hamillrobin@gmail.com>
2016-10-16 22:08:53 -0400

    Add some styles Play with Bourbon Check out bracket-less SASS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('posts.created_at DESC')
   end
 
   # GET /posts/1
