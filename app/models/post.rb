97c523f48327be88b48b9e61d80f7219ede6a68e
Robin Hamill <hi@robinhamill.ca>
2016-10-30 11:40:37 -0400

    Update styles Add hero banner Refactor blog index items

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
 	has_attached_file :lead_image,
 		:default_url => "_.jpg",
 		styles: {
-			medium: "300x300>",
+			full: "1200x1200",
+			large: "800x800",
+			medium: "400x400>",
 			thumb: "100x100>" }
   validates_attachment_content_type :lead_image, content_type: /\Aimage\/.*\z/
 
