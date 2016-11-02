45e67ef57ea26092eec6977b7d6cd1f2c0bc4910
Robin Hamill <hamillrobin@gmail.com>
2016-11-02 14:31:10 -0400

    Remove un-needed code blocks from Paloma

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,11 +1,5 @@
 var PostsController = Paloma.controller('Posts');
 
-PostsController.prototype.new = function(){
-};
-
-PostsController.prototype.edit = function(){
-};
-
 PostsController.prototype.show = function(){
 	(function(){
 	    Rainbow.color();
