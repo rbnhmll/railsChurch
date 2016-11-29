ee53cf3f8b8a88a3c90e419006cc1dd4c8e72f83
Robin Hamill <hamillrobin@gmail.com>
2016-11-29 11:23:27 -0500

    New string

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def letsencrypt
-    render text: "USQhFCKtdluw4f1Xm-GEXBIhinIIperHIxXzPTfsmbA"
+    render text: "NO3FFTlCBhdRgiZSemJp2VCq125w8KOh8YSbQm1yrbc.eEZrxiV3ja37dqBtcmbdUhFT4DhRjMMVXxrq7B9_7kw"
   end
 
 end
