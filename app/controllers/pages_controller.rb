b988143486207c766c5562bba30301700ff977f9
Robin Hamill <hamillrobin@gmail.com>
2016-11-29 11:28:46 -0500

    Update string

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def letsencrypt
-    render text: "NO3FFTlCBhdRgiZSemJp2VCq125w8KOh8YSbQm1yrbc.eEZrxiV3ja37dqBtcmbdUhFT4DhRjMMVXxrq7B9_7kw"
+    render text: "GBxqRcdMcNQFVRpcHuWd_xdD3cjVPUpJQmkpvKX0GYc.eEZrxiV3ja37dqBtcmbdUhFT4DhRjMMVXxrq7B9_7kw"
   end
 
 end
