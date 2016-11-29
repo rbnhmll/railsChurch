553431b06519b3258f4df172014bcb3a6686457b
Robin Hamill <hamillrobin@gmail.com>
2016-11-29 11:36:25 -0500

    Remove certbot setup

--- app/controllers/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class PagesController < ApplicationController
-
-  def letsencrypt
-    render text: "GBxqRcdMcNQFVRpcHuWd_xdD3cjVPUpJQmkpvKX0GYc.eEZrxiV3ja37dqBtcmbdUhFT4DhRjMMVXxrq7B9_7kw"
-  end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, controllers: { registrations: "registrations"}
 
-  get '/.well-known/acme-challenge/:id' => 'pages#letsencrypt'
   root to: "posts#index"
 
 end
