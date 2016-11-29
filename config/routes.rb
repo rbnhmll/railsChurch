6c66843c65caf63ff2a94ef4a60a1a0355b7d844
Robin Hamill <hamillrobin@gmail.com>
2016-11-29 10:54:53 -0500

    Let's Encrypt Setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, controllers: { registrations: "registrations"}
 
+  get '/.well-known/acme-challenge/:id' => 'posts#letsencrypt'
   root to: "posts#index"
 
 end
