2a35278bc44359d933fedf62ca14a568b2796f79
Robin Hamill <hi@robinhamill.ca>
2017-01-05 12:01:00 -0500

    Setup Contributor details Actions, model validations, form, etc.

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,8 +1,26 @@
 class ContributorsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :new, :edit, :destroy]
+  before_action :set_contributor, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def new
+  	@contributor = Contributor.new
+  end
+
+  def create
+  	@contributor = Contributor.new(contributor_params)
+
+    respond_to do |format|
+      if @contributor.save
+        format.html { redirect_to @contributor, notice: 'contributor was successfully created.' }
+        format.json { render :show, status: :created, location: @contributor }
+      else
+        format.html { render :new }
+        format.json { render json: @contributor.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def edit
@@ -10,4 +28,24 @@ class ContributorsController < ApplicationController
 
   def show
   end
+
+  def destroy
+  	@contributor.destroy
+    respond_to do |format|
+      format.html { redirect_to contributors_url, notice: 'Contributor was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_contributor
+      @contributor = Contributor.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def contributor_params
+      params.require(:contributor).permit(:name, :email, :bio, :avatar)
+    end
+
 end
--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,2 +1,10 @@
 class Contributor < ActiveRecord::Base
+	has_attached_file :avatar,
+		:default_url => "anon.jpg",
+		styles: {
+			large: "800x800",
+			medium: "400x400>",
+			thumb: "200x200>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates :name, presence: true
 end
