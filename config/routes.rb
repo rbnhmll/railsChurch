0abb6d347ceeba7074e30a34bde58ecdbe4ff8c1
Robin Hamill <hi@robinhamill.ca>
2017-01-05 11:40:30 -0500

    Add contributors controller Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+
   get 'pages/about'
   resources :posts
+  resources :contributors
   devise_for :users, controllers: { registrations: "registrations"}
 
   root to: "posts#index"
